d53e0778d3f6ea703f9a9fd66ddd2c5d18fe734d
sbabureddy09123456 <94162906+sbabureddy09123456@users.noreply.github.com>
2022-09-05 22:18:47 +0530

    code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar.js"
 function App() {
   return (
     <div className="App">
-      <h1>shey pizza</h1>
+      <Navbar />
     </div>
   );
 }
