22f85cebe2410cc4a06c0fb8a10e0358c9b7cf9e
sbabureddy09123456 <94162906+sbabureddy09123456@users.noreply.github.com>
2022-09-05 21:22:47 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+      <h1>shey pizza</h1>
+    </div>
+  );
+}
+
+export default App;
